8b799a8e4ac4d69a0d2e65ec50aa04c3f8e32d30
InnaBoby <146350138+InnaBoby@users.noreply.github.com>
2024-04-10 15:10:08 +0300

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -67,7 +67,7 @@ def salary_dinamic_graph (df, df2=False):
   # добавим график среднего уровня зарплат по 4-ем отраслям
   ax1.plot(years, avarage_salary, label='avarage_salary', linestyle='--')
   ax1.set_xlabel('years')
-  plt.xticks(rotation='vertical')
+  plt.xticks(rotation=60)
   ax1.set_ylabel('salary')
   ax1.set_title("Изменение зарплаты по годам")
   ax1.legend()
@@ -84,8 +84,9 @@ def salary_dinamic_graph (df, df2=False):
 
 
 def real_salary_graph(year, inflation, salary):
-    
-''' Функция для добавления сравнения с прошлым годом номинальной и реальной зп'''
+
+''' Функция для добавления сравнения с прошлым годом номинальной и реальной зп '''
+
     year = int(year)
     infl = inflation['Всего'] [inflation['Год'] == year].item()
     correct_on_infl = salary[str(year)] * ((100 - infl) / 100)
@@ -118,44 +119,43 @@ def real_salary_graph(year, inflation, salary):
 def add_prirost(salary):
     
 ''' Функция для добавления графика прироста'''
-    
-  salary2 = salary.T
-  salary2.columns = salary2.iloc[0]
-  salary2 = salary2[1:]
-  salary2['Avarage'] = (salary2['добыча полезных ископаемых'] +
+    salary2 = salary.T
+    salary2.columns = salary2.iloc[0]
+    salary2 = salary2[1:]
+    salary2['Avarage'] = (salary2['добыча полезных ископаемых'] +
                         salary2['обрабатывающие производства'] +
                         salary2['образование'] +
                         salary2['строительство']) / 4
-  for col in salary2.columns.to_list():
-    salary2[f'Прирост ' + col] = 0
-    for indx in range(1, len(salary2)):
-      salary2[f'Прирост ' + col][indx] = (salary2[col][indx] * 100 / salary2[col][indx - 1]) - 100
+    for col in salary2.columns.to_list():
+        salary2[f'Прирост ' + col] = 0
+        for indx in range(1, len(salary2)):
+          salary2[f'Прирост ' + col][indx] = (salary2[col][indx] * 100 / salary2[col][indx - 1]) - 100
 
-  salary2 = salary2.T
+    salary2 = salary2.T
 
-  years = salary2.columns.to_list()
-  fig, ax1 = plt.subplots()
-  color = 'tab:blue'
-  for indx in range(len(salary2[:5])):
-    legend_data = salary2.index.to_list()[indx]
-    ax1.plot(years, salary2.iloc[indx].to_list(), label=legend_data, linestyle='--')
-  ax1.set_xlabel('years', color=color)
-  ax1.tick_params(axis='y', labelcolor=color)
-  ax1.tick_params(axis='x', labelcolor=color)
-  plt.xticks(rotation='vertical')
-  ax1.set_ylabel('Средняя зарплата, в руб.', color=color)
-  ax1.set_title("Изменение зарплаты по годам")
+    years = salary2.columns.to_list()
+    fig, ax1 = plt.subplots()
+    color = 'tab:blue'
+    for indx in range(len(salary2[:5])):
+        legend_data = salary2.index.to_list()[indx]
+        ax1.plot(years, salary2.iloc[indx].to_list(), label=legend_data, linestyle='--')
+    ax1.set_xlabel('years', color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.tick_params(axis='x', labelcolor=color)
+    plt.xticks(rotation=60)
+    ax1.set_ylabel('Средняя зарплата, в руб.', color=color)
+    ax1.set_title("Изменение зарплаты по годам")
 
-  ax2 = ax1.twinx()
-  color = 'tab:red'
-  ax2.set_ylabel('Прирост к рошлому году, в %', color=color)
-  ax2.tick_params(axis='y', labelcolor=color)
-  for indx in range(5, len(salary2)):
-    legend_data = salary2.index.to_list()[indx]
-    ax2.plot(years, salary2.iloc[indx].to_list(), label=legend_data)
-  ax2.legend()
-
-  return fig
+    ax2 = ax1.twinx()
+    color = 'tab:red'
+    ax2.set_ylabel('Прирост к рошлому году, в %', color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+    for indx in range(5, len(salary2)):
+        legend_data = salary2.index.to_list()[indx]
+        ax2.plot(years, salary2.iloc[indx].to_list(), label=legend_data)
+    ax2.legend()
+    
+    return fig
 
 
 def add_real(salary):
@@ -184,7 +184,7 @@ def add_real(salary):
     ax1.set_xlabel('years', color=color)
     ax1.tick_params(axis='y', labelcolor=color)
     ax1.tick_params(axis='x', labelcolor=color)
-    plt.xticks(rotation='vertical')
+    plt.xticks(rotation=60)
     ax1.set_ylabel('Средняя зарплата, в руб.', color=color)
     ax1.set_title("Изменение зарплаты по годам")
     ax1.legend()
